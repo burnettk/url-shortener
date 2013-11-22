165a8a801936e8d39ff5bfc9b5762009f8dd1c9a
burnettk <burnettk@gmail.com>
2013-11-22 14:15:18 -0500

    better headers, and some other styles. fix js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,3 +4,23 @@
 // //= require_tree .
 
 $(document).foundation();
+
+if ('#shortcut_shortcut') {
+	var MakeClearWhatIsHappening = {
+	now: function(event) {
+	  var shortcut = $('#shortcut_shortcut').val();
+	  var url = $('#shortcut_url').val();
+	  if (url === 'http://') url += '?';
+	  if (shortcut.length == 0) shortcut = '?';
+	  if (url.length == 0) url = '?';
+	  $('#notes_to_user').html("When you go to <strong>" + window.location.protocol + "//" + window.location.host + "/" + shortcut + "</strong> in your browser, you will be taken to <strong>" + url + "</strong>");
+	}
+	}
+
+	MakeClearWhatIsHappening.now();
+
+	$('#shortcut_shortcut').focus();
+
+	$('#shortcut_shortcut').on('keyup', MakeClearWhatIsHappening.now);
+	$('#shortcut_url').on('keyup', MakeClearWhatIsHappening.now);
+}
